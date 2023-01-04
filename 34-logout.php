1a841484f4b3d9f1213696702d55c4acb654c364
DawidJ <106882138+dawidjach@users.noreply.github.com>
2023-01-04 14:48:49 +0100

    Create 34-logout.php

---
+++ 34-logout.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+require_once( 'functions.inc.php' );
+$database = 'blog';
+require_once( 'pdo-connect.inc.php' );
+
+$page_title = 'Blog - Logout';
+$page_header = NULL;
+
+// Session leeren
+$_SESSION = array();
+// Session zerstören
+if( session_destroy() ) {
+    $message = '<p class="alert alert-success">Sie wurden erfolgreich ausgeloggt.</p>';
+} else {
+    $message = '<p class="alert alert-danger">Ausloggen hat nicht funktioniert... Viel Glück!</p>';
+}
+
+include_once '_header.php';
+
+echo $message;
+
+?>
+    
+<?php get_footer(); ?>
